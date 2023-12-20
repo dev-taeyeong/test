8679ad7a5502424d0ff4bdf2da3d8525597ca248
dev-taeyeong <dev.taeyeong@gmail.com>
2023-12-20 23:47:47 +0900

    commit

---
+++ src/main/java/com/example/ebtest/EbTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.ebtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbTestApplication.class, args);
+    }
+
+}
