edc020385b2b988cf79dafeee5e4b01206e12b88
Andy <andy@everex.co.kr>
2023-12-21 12:52:12 +0900

    test commit

--- src/main/java/com/example/ebtest/controller/HelloController.java
+++ src/main/java/com/example/ebtest/controller/HelloController.java
@@ -4,10 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestController {
+public class HelloController {
 
     @GetMapping
-    public String test() {
-        return "test";
+    public String Hello() {
+        return "Hello, World!";
     }
 }
